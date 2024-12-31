335a2cf5c1f4101a33637d627ed62be332ebe002
benshabbat <benshabbat27@gmail.com>
2024-12-31 15:14:13 +0200

    made table for colors

--- Pages/Colors/Index.cshtml.cs
+++ Pages/Colors/Index.cshtml.cs
@@ -4,15 +4,45 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 
 namespace CrudCSharpSql.Pages.Colors
 {
     public class Index : PageModel
     {
-
+        public List<ColorInfo> ColorsList { get; set; } = [];
         public void OnGet()
         {
+            try
+            {
+                string connectionString = @"Server=localhost\SQLEXPRESS;Database=crmdb;Trusted_Connection=True;TrustServerCertificate=True;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM colors";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ColorInfo color = new ColorInfo();
+                                color.Id = reader.GetInt32(0);
+                                color.Name = reader.GetString(1);
+                                color.Price = reader.GetDouble(2);
+                                color.IsInStock = reader.GetBoolean(3);
+                                ColorsList.Add(color);
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in fetching data from database" + ex.Message);
+            }
         }
     }
 
@@ -20,7 +50,7 @@ namespace CrudCSharpSql.Pages.Colors
     {
         public int Id { get; set; }
         public string Name { get; set; } = "";
-        
+
         public double Price { get; set; } = 0.0;
 
         public bool IsInStock { get; set; } = false;
--- obj/Debug/net8.0/CrudCSharpSql.AssemblyInfo.cs
+++ obj/Debug/net8.0/CrudCSharpSql.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudCSharpSql")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d3ef03168c7358aba3b94fbc380418131b60003")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19935a3cfb808306a03bfbf863f4d28a013270a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudCSharpSql")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudCSharpSql")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
